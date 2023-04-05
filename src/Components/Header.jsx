b5b0abcabfd07d06bdeb7438cdb5f030f35e3f58
quang anh <anhkhongdeptrai090@gmail.com>
2023-04-06 00:46:17 +0700

    Changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,6 +13,7 @@ import { CgClose } from "react-icons/cg";
 
 import { Context } from "../Context/contextApi";
 import Loader from "../Shared/loader";
+import LeftNav from "./LeftNav";
 
 
 const Header = () => {
@@ -32,19 +33,24 @@ const Header = () => {
   };
 
   const mobileMenuToggle = () => {
-    setMobileMenu(!mobileMenu)
+    setMobileMenu(!mobileMenu);
+    console.log('Trạng thái hiện tại', mobileMenu)
   };
 
   const { pathname } = useLocation();
   const pageName = pathname?.split("/").filter(Boolean)?.[0];
   return (
-    <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black">
+    <div className="sticky top-0 z-100 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black">
       {loading && <Loader />}
       <div className="flex h-5 items-center">
           {pageName !== "video" && 
           (<div className="flex md:hidden md:mr-6 cursor-pointer items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]"
            onClick={mobileMenuToggle}>
-            {mobileMenu ? (<CgClose className="text-white text-xl" />) : (<SlMenu className="text-white text-xl" />)}
+            {mobileMenu ? (
+              <>
+                <CgClose className="text-white text-xl"  />
+              </>
+            ) : (<SlMenu className="text-white text-xl" />)}
            </div>
           )}
           <Link to="/" className="flex h-5 items-center">
