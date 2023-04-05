cda2bbca75f49384c56a7268a027ec1578c962c3
quang anh <anhkhongdeptrai090@gmail.com>
2023-04-05 23:38:27 +0700

    Changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -75,7 +75,9 @@ const Header = () => {
                               />
                             </div>
                               <button className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] 
-                                                  rounded-r-3xl bg-white/[0.1]"> 
+                                                  rounded-r-3xl bg-white/[0.1]"
+                                                  onClick={() => searchQueryHandlder("searchButton")}
+                                                  > 
                               <IoIosSearch className="text-white text-xl" /></button>
 
           </div>
